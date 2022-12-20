695974e75c1e55ecf94a8a1fa4bd87e00a7f3d14
Darshan <darshan.android@gmail.com>
2022-12-20 11:34:09 +0000

    add functionality to read CSV and its unit testing

---
+++ src/main/java/com/test/addressbook/repository/impl/CSVAddressBookRepoImpl.java
@@ -0,0 +1,83 @@
+package com.test.addressbook.repository.impl;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.test.addressbook.model.Person;
+import com.test.addressbook.repository.AddressBookRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Repository;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Repository
+@Qualifier("csvAddressBookRepo")
+public class CSVAddressBookRepoImpl implements AddressBookRepo {
+
+    @Value("${address.csv.path}")
+    Resource pathToCSV;
+    @Override
+    public List<Person> getListOfPerson(){
+        List<Person> listOfPerson;
+        try {
+            listOfPerson =  this.readCSVLineByLine(pathToCSV);
+        } catch (Exception e) {
+            log.error("Unexpected error occurred. Message: {}, caused by {}",
+                    e.getMessage(), e.getCause());
+            throw new RuntimeException(e);
+        }
+        return listOfPerson;
+    }
+
+    private final DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendPattern("dd/MM/")
+            .optionalStart()
+            .appendPattern("uuuu")
+            .optionalEnd()
+            .optionalStart()
+            .appendValueReduced(ChronoField.YEAR, 2, 2, 1920)
+            .optionalEnd()
+            .toFormatter();
+
+    private List<Person> readCSVLineByLine(final Resource filePath) throws Exception {
+
+        CSVParser parser = new CSVParserBuilder()
+                .withSeparator(',')
+                .withIgnoreQuotations(true)
+                .build();
+
+        List<Person> listOfPerson = new ArrayList<>();
+
+        try (Reader reader = new InputStreamReader(filePath.getInputStream())) {
+            CSVReaderBuilder csvReaderBuilder = new CSVReaderBuilder(reader)
+                    .withCSVParser(parser);
+
+            try (CSVReader csvReader = csvReaderBuilder.build()) {
+                String[] csvRecord;
+                while ((csvRecord = csvReader.readNext()) != null) {
+                    Person person = Person.builder()
+                            .name(StringUtils.strip(csvRecord[0]))
+                            .gender(Person.GenderEnum.valueOf(StringUtils.strip(csvRecord[1]).toUpperCase()))
+                            .dateOfBirth(LocalDate.parse(StringUtils.strip(csvRecord[2]), formatter))
+                            .build();
+                    person.setName(csvRecord[0].trim());
+                    listOfPerson.add(person);
+                }
+            }
+        }
+        return listOfPerson;
+    }
+
+}
---
+++ src/test/java/com/test/addressbook/repository/CSVAddressBookRepoImplTest.java
@@ -0,0 +1,52 @@
+package com.test.addressbook.repository;
+
+import com.test.addressbook.model.Person;
+import com.test.addressbook.repository.impl.CSVAddressBookRepoImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+public class CSVAddressBookRepoImplTest {
+    @InjectMocks
+    private CSVAddressBookRepoImpl csvAddressBookRepo;
+    @BeforeEach
+    public void init() {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:/csv/addressbook");
+        ReflectionTestUtils.setField(csvAddressBookRepo, "pathToCSV", resource);
+    }
+
+    @Test
+    public void testGetListOfPerson(){
+
+        List<Person> personList  = new ArrayList<>();
+
+        Person person1 = Person.builder().name("John Doe")
+                .dateOfBirth(LocalDate.of(1999, 12, 01)).gender(Person.GenderEnum.MALE).build();
+
+        Person person2 = Person.builder().name("Jane Doe")
+                .dateOfBirth(LocalDate.of(1970, 11, 01)).gender(Person.GenderEnum.FEMALE).build();
+
+        personList.add(person1);
+        personList.add(person2);
+
+        List<Person> result = csvAddressBookRepo.getListOfPerson();
+        assertEquals(personList, result);
+    }
+
+}
